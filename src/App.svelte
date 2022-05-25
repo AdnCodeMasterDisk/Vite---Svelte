59fbbf386b93ce744ac0b49ed3647cb580520271
adncode <deepartesdigitales@gmail.com>
2022-05-25 11:36:28 -0500

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import RouterView from "./router/router.svelte";
+  import "./assets/style.css"
+</script>
+
+<RouterView/>
+
+<style>
+
+</style>
---
+++ src/router/router.svelte
@@ -0,0 +1,22 @@
+<script>
+    import {Router,Link,Route,createMemorySource,createHistory,} 
+    from "svelte-navigator";
+    import homeView from "../views/home.svelte";
+    import aboutView from "../views/about.svelte";
+
+    const history = createHistory(createMemorySource());
+</script>
+
+
+<Router {history}>
+
+    <div class="menu">
+        <Link class="itemMenu" to="/">Home</Link>
+        <Link class="itemMenu" to="about">About</Link>
+    </div>
+
+    <main>
+        <Route path="/" component="{homeView}" />
+        <Route path="about" component="{aboutView}" />
+    </main>
+</Router>
